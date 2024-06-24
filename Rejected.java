37b3d282992be45b460b4ab18aecb1a2df002fde
TumukundeAndrew <tumukundeandrew07@gmail.com>
2024-06-24 14:33:09 +0300

    Created a loop for iterating through the rejected

--- Rejected.java
+++ Rejected.java
@@ -1,3 +1,10 @@
 public class Rejected {
-    
-}
+    for(int i=0; i<rejectedStudents.length;i++){
+        System.out.println('Student'(i+1));
+        System.out.println(rejectedStudents[i].firstName);
+        System.out.println(rejectedStudents[i].lastName);
+        System.out.println(rejectedStudents[i].email);
+        System.out.println(rejectedStudents[i].regNo);   
+        System.out.println('-----------------------------------------------------------------------------------------'));
+    }
+   
