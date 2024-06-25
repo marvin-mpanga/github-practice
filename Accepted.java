f46d38d32b978f14ba6ff3bea316f2e7344fbd5f
marvin-mpanga <mpangamarvin2003@gmail.com>
2024-06-25 12:46:54 +0300

    added a new line

--- Accepted.java
+++ Accepted.java
@@ -1,3 +1,12 @@
 public class Accepted {
-    
+    public static void printAcceptedStudents(StudentRegistration.Student[] rejectedStudents) {
+        for (int i = 0; i < acceptedStudents.length; i++) {
+            System.out.println("Accepted Student " + (i + 1));
+            System.out.println("First Name: " + acceptedStudents[i].firstName);
+            System.out.println("Last Name: " + acceptedStudents[i].lastName);
+            System.out.println("Email: " + acceptedStudents[i].email);
+            System.out.println("Registration Number: " + acceptedStudents[i].regNo);
+            System.out.println("-----------------------------------------------------------------------------------------");
+        }
+    }
 }
